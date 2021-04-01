c395cec289ac978abda740ba3fc090e6c25f26fd
crypt0 <acvjetan@gmail.com>
2021-04-01 13:08:55 +0200

    Add request with context.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = "Hello Manny"
-})
+// Add a date method to the context
+app.context.date = Date();
+
+// Response
+app.use((ctx) => {
+  // Use the state
+  ctx.state.user = "Alex";
+
+  // Request object usage
+  let from = ctx.request.origin;
+
+  // Print out date
+  ctx.body = `Hello ${ctx.state.user} on ${ctx.date}`;
+  console.log(from);
+});
 
 app.listen(3001);
