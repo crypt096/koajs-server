08acac4fde593806b122b9b1440468de7242cec3
crypt0 <acvjetan@gmail.com>
2021-04-02 16:23:49 +0200

    Finalize cascading.

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ app.context.userData = {
   occupation: "Software Engineer",
 };
 
-// Response
-app.use(async (ctx) => {
-  ctx.response.body = await ctx.userData;
-});
-
 // log
 app.use(async (ctx, next) => {
   await next();
   const responseTime = ctx.response.get("X-Response-Time");
   console.log(`${ctx.request.method} ${ctx.request.url} - ${responseTime}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const milisecond = Date.now() - start;
+  ctx.set("X-Response-Time", `${milisecond} ms`);
+});
+
+// Response
+app.use(async (ctx) => {
+  ctx.response.body = await ctx.userData;
 });
 
 app.listen(3001);
